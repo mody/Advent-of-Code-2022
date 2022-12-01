5663587ce85d655ff4f5b4d31b4aca3e64d88406
Patrik Modesto <patrik.modesto@gmail.com>
2022-12-01 09:40:15 +0100

    day1 - part2

--- src/day1.cc
+++ src/day1.cc
@@ -1,17 +1,21 @@
+#include <cassert>
 #include <fmt/core.h>
 #include <iostream>
+#include <set>
 #include <string>
 
 int main()
 {
-    uint64_t top = 0, val = 0, sum = 0;
+    uint64_t val = 0, sum = 0;
+
+    std::set<uint64_t, std::greater<uint64_t>> top;
 
     std::string line;
     while (std::getline(std::cin, line))
     {
         if (line.empty())
         {
-            top = std::max(top, sum);
+            top.insert(sum);
             sum = 0;
         }
         else
@@ -20,9 +24,16 @@ int main()
             sum += val;
         }
     }
-    top = std::max(top, sum);
+    top.insert(sum);
 
-    fmt::print("1: {}\n", top);
+    auto it = top.begin();
+    fmt::print("1: {}\n", *it);
+
+    assert(top.size() > 2);
+    sum = *it++;
+    sum += *it++;
+    sum += *it++;
+    fmt::print("2: {}\n", sum);
 
     return 0;
 }
