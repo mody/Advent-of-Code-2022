cb5eb3d102bc357a7a9b8c3d5a364e39a7edfbb9
Patrik Modesto <pmodesto@idirect.net>
2022-12-06 09:20:54 +0100

    day6 - par2

--- src/day6.cc
+++ src/day6.cc
@@ -1,22 +1,38 @@
+#include <array>
 #include <cassert>
 #include <fmt/core.h>
 #include <iostream>
 #include <string>
 
-void part1(std::string const& input)
+template<unsigned int SEGMENT>
+void part(std::string const& input)
 {
-    size_t from = 0;
-    for (; (from + 4) < input.size(); ++from) {
-        if (input.at(from) != input.at(from + 1)
-            && input.at(from) != input.at(from + 2)
-            && input.at(from) != input.at(from + 3)
-            && input.at(from + 1) != input.at(from + 2)
-            && input.at(from + 1) != input.at(from + 3)
-            && input.at(from + 2) != input.at(from + 3))
-        {
-            fmt::print("1: {}\n", from + 4);
+    std::array<unsigned int, 'z' - 'a' + 1> freq_data {};
+
+    auto freq_unique = [&freq_data]() -> bool {
+        for (auto const& f : freq_data) {
+            if (f > 1) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    auto freq_index = [](unsigned char c) -> unsigned int {
+        return c - 'a';
+    };
+
+    for (unsigned int i = 0; i < SEGMENT; ++i) {
+        freq_data.at(freq_index(input.at(i))) += 1;
+    }
+
+    for (size_t from = 0; (from + SEGMENT) < input.size(); ++from) {
+        if (freq_unique()) {
+            fmt::print("1: {}\n", from + SEGMENT);
             break;
         }
+        freq_data.at(freq_index(input.at(from))) -= 1;
+        freq_data.at(freq_index(input.at(from + SEGMENT))) += 1;
     }
 }
 
@@ -28,7 +44,8 @@ int main()
         if (line.empty()) {
             break;
         }
-        part1(line);
+        part<4>(line);
+        part<14>(line);
     }
 
     return 0;
