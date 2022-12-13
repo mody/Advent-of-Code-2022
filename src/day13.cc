3c5f489edaeed32c5be74207434466228e4d43f5
Patrik Modesto <patrik.modesto@gmail.com>
2022-12-13 21:29:03 +0100

    day13 - par2

--- src/day13.cc
+++ src/day13.cc
@@ -1,7 +1,9 @@
+#include <algorithm>
 #include <bit>
 #include <cassert>
 #include <fmt/core.h>
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
 
@@ -20,6 +22,55 @@ void make_list(std::string& s, size_t pos)
     s.insert(pos, 1, ']');
 }
 
+bool compare_less(std::string lhs, std::string rhs)
+{
+    size_t pos1 = 0, pos2 = 0;
+
+    for (;;) {
+        if (pos1 >= lhs.size()) {
+            return true;
+        }
+        if (pos2 >= rhs.size()) {
+            return false;
+        }
+
+        char c1 = lhs.at(pos1);
+        char c2 = rhs.at(pos2);
+
+        if (c1 == '[' && c2 == '[') {
+            ++pos1;
+            ++pos2;
+        } else if (c1 == ']' && c2 == ']') {
+            ++pos1;
+            ++pos2;
+        } else if (c1 == ',' && c2 == ',') {
+            ++pos1;
+            ++pos2;
+        } else if (c1 >= '0' && c1 <= '9' && c2 >= '0' && c2 <= '9') {
+            size_t next1 = 0, next2 = 0;
+            auto n1 = std::stoi(&lhs[pos1], &next1);
+            auto n2 = std::stoi(&rhs[pos2], &next2);
+            if (n1 > n2) {
+                return false;
+            } else if (n1 < n2) {
+                return true;
+            }
+            pos1 += next1;
+            pos2 += next2;
+        } else if (c1 == '[' && c2 >= '0' && c2 <= '9') {
+            make_list(rhs, pos2);
+        } else if (c1 >= '0' && c1 <= '9' && c2 == '[') {
+            make_list(lhs, pos1);
+        } else if (c2 == ']') {
+            return false;
+        } else if (c1 == ']') {
+            return true;
+        } else {
+            assert(false);
+        }
+    }
+}
+
 void part1(Data const& data)
 {
     unsigned score = 0;
@@ -27,86 +78,36 @@ void part1(Data const& data)
 
     for (auto [lhs, rhs] : data) {
         ++idx;
-        // fmt::print("{} - {} vs {}\n", idx, lhs, rhs);
-
-        size_t pos1 = 0, pos2 = 0;
-
-        for(;;) {
-            // std::cout.flush();
-            if (pos1 >= lhs.size()) {
-                // fmt::print("lhs ends XXX\n");
-                score += idx;
-                break;
-            }
-            if (pos2 >= rhs.size()) {
-                // fmt::print("rhs ends\n");
-                break;
-            }
-
-            char c1 = lhs.at(pos1);
-            char c2 = rhs.at(pos2);
-
-            if (c1 == '[' && c2 == '[') {
-                // fmt::print("same [\n");
-                ++pos1;
-                ++pos2;
-            } else if (c1 == ']' && c2 == ']') {
-                // fmt::print("same ]\n");
-                ++pos1;
-                ++pos2;
-            } else if (c1 == ',' && c2 == ',') {
-                // fmt::print("same ,\n");
-                ++pos1;
-                ++pos2;
-            } else if (c1 >= '0' && c1 <= '9' && c2 >= '0' && c2 <= '9') {
-                size_t next1 = 0, next2 = 0;
-                auto n1 = std::stoi(&lhs[pos1], &next1);
-                auto n2 = std::stoi(&rhs[pos2], &next2);
-                if (n1 > n2) {
-                    // fmt::print("{} > {}\n", n1, n2);
-                    break;
-                } else if (n1 < n2) {
-                    // fmt::print("{} < {} XXX\n", n1, n2);
-                    score += idx;
-                    break;
-                }
-                // fmt::print("{} <= {}\n", n1, n2);
-                // fmt::print("pos1: {}, next1: {}\n", pos1, next1);
-                // fmt::print("pos2: {}, next2: {}\n", pos2, next2);
-                pos1 += next1;
-                pos2 += next2;
-            } else if (c1 == '[' && c2 >= '0' && c2 <= '9') {
-                make_list(rhs, pos2);
-                // fmt::print("make_list rhs - {}\n", rhs);
-            } else if (c1 >= '0' && c1 <= '9' && c2 == '[') {
-                make_list(lhs, pos1);
-                // fmt::print("make_list lhs - {}\n", lhs);
-            } else if (c2 == ']') {
-                // fmt::print("rhs out of items\n");
-                break;
-            } else if (c1 == ']') {
-                // fmt::print("lhs out of items XXX\n");
-                score += idx;
-                break;
-            } else {
-                // fmt::print("lhs: {}, c1: {}, pos1: {}\n", lhs, c1, pos1);
-                // fmt::print("rhs: {}, c2: {}, pos2: {}\n", rhs, c2, pos2);
-                // std::cout.flush();
-                assert(false);
-            }
+        if (compare_less(std::move(lhs), std::move(rhs))) {
+            score += idx;
         }
     }
-
-    // 5757 -- too low
 
     fmt::print("1: {}\n", score);
 }
 
+void part2(Data const& data)
+{
+    std::vector<std::string> v;
+    v.push_back("[[2]]");
+    v.push_back("[[6]]");
+
+    for (auto [lhs, rhs] : data) {
+        v.push_back(std::move(lhs));
+        v.push_back(std::move(rhs));
+    }
+
+    std::sort(v.begin(), v.end(), [](const std::string& lhs, const std::string& rhs) {
+        return compare_less(lhs, rhs);
+    });
+
+    auto i1 = std::find(v.begin(), v.end(), "[[2]]");
+    auto i2 = std::find(v.begin(), v.end(), "[[6]]");
+    fmt::print("2: {}\n", ((1 + std::distance(v.begin(), i1)) * (1 + std::distance(v.begin(), i2))));
+}
+
 int main()
 {
-    // [1,1,3,1,1]
-    // [1,1,5,1,1]
-
     Data data;
 
     std::string line;
@@ -129,6 +130,7 @@ int main()
     }
 
     part1(data);
+    part2(data);
 
     return 0;
 }
